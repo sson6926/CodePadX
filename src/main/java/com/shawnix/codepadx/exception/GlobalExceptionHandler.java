f3fb0e202a890a48b23ff84a44138eb825d5683b
SonNguyen <sson6926@gmail.com>
2026-03-18 14:44:38 +0700

    Add valid exception handler

--- src/main/java/com/shawnix/codepadx/exception/GlobalExceptionHandler.java
+++ src/main/java/com/shawnix/codepadx/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.shawnix.codepadx.exception;
 
 import com.shawnix.codepadx.dto.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,5 +23,14 @@ public class GlobalExceptionHandler {
     ResponseEntity<ApiResponse> handlingAppException(AppException exception) {
         return ResponseEntity.badRequest().body(ApiResponse.builder().code(exception.getErrorCode().getCode()).data(exception.getErrorCode().getMessage()).build());
 
+    }
+
+    @ExceptionHandler(exception = MethodArgumentNotValidException.class)
+    ResponseEntity<ApiResponse> handleValidationException(MethodArgumentNotValidException exception) {
+        String errorMessage = exception.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .findFirst()
+                .orElse("Validation error");
+        return ResponseEntity.badRequest().body(ApiResponse.builder().code(400).data(errorMessage).build());
     }
 }
