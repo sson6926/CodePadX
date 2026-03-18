10b29ce8959b1274f1d370baae90b5aa9b471b01
SonNguyen <sson6926@gmail.com>
2026-03-18 13:43:47 +0700

    add user entity

---
+++ src/main/java/com/shawnix/codepadx/entity/User.java
@@ -0,0 +1,30 @@
+package com.shawnix.codepadx.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String username;
+    private String email;
+    private String password;
+    private String photo;
+    private String role;
+    private LocalDateTime createdAt;
+}
