8486194eb516f5d5d744fa2670ea53b9273bbf27
SonNguyen <sson6926@gmail.com>
2026-03-18 13:34:38 +0700

    first commit

---
+++ src/main/java/com/shawnix/codepadx/CodePadXApplication.java
@@ -0,0 +1,13 @@
+package com.shawnix.codepadx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePadXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodePadXApplication.class, args);
+    }
+
+}
