711c082b46fde86d6f5061333675cbeda84d15b7
SonNguyen <sson6926@gmail.com>
2026-03-18 16:02:17 +0700

    add code entity

---
+++ src/main/java/com/shawnix/codepadx/entity/Code.java
@@ -0,0 +1,48 @@
+package com.shawnix.codepadx.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "codes")
+public class Code {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @ColumnDefault("Untitled")
+    private String title;
+
+    @Lob
+    @Column(nullable = false)
+    private String sourceCode;
+
+    @Lob
+    private String input;
+
+    @ManyToOne
+    @JoinColumn(name = "language_id", nullable = false)
+    private Language language;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
