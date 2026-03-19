16042c6ca584c7810656c3db66c2548b9d1d7b72
SonNguyen <sson6926@gmail.com>
2026-03-19 16:09:51 +0700

    handle exception for file/folder

--- src/main/java/com/shawnix/codepadx/service/executor/TempFileService.java
+++ src/main/java/com/shawnix/codepadx/service/executor/TempFileService.java
@@ -7,17 +7,26 @@ import java.util.Random;
 import java.util.UUID;
 
 public class TempFileService {
-    public static String createTempFile(String dir, String content, String fileName) throws IOException {
-        String filePath = dir + "/" + fileName;
-        Files.writeString(Path.of(filePath), content);
-        return filePath;
+    public static String createTempFile(String dir, String content, String fileName) {
+        try {
+            String filePath = dir + "/" + fileName;
+            Files.writeString(Path.of(filePath), content);
+            return filePath;
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to create temp file", e);
+        }
     }
 
-    public static String createTempDir() throws IOException {
-        String baseDir = "/tmp";
-        String dir = baseDir + "/code-run-" + UUID.randomUUID();
-        Files.createDirectories(Path.of(dir));
-        return dir;
+    public static String createTempDir() {
+        try {
+            String baseDir = "/tmp";
+            String dir = baseDir + "/code-run-" + UUID.randomUUID();
+            Files.createDirectories(Path.of(dir));
+            return dir;
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to create temp directory", e);
+        }
+
     }
 
     public static void deleteTempDir(String dir) throws IOException {
