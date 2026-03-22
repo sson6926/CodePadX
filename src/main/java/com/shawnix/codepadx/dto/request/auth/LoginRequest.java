42d909cc2c1108025162b28d2bcd2f1f47510256
SonNguyen <sson6926@gmail.com>
2026-03-22 21:26:44 +0700

    dto for login

--- src/main/java/com/shawnix/codepadx/dto/request/auth/LoginRequest.java
+++ src/main/java/com/shawnix/codepadx/dto/request/auth/LoginRequest.java
@@ -3,9 +3,11 @@ package com.shawnix.codepadx.dto.request.auth;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class LoginRequest {
     String email;
