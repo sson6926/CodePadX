29c771b4d069d8c622bf8d8f87f9567ded14c3f2
SonNguyen <sson6926@gmail.com>
2026-03-22 09:34:37 +0700

    auth

--- src/main/java/com/shawnix/codepadx/repository/UserRepository.java
+++ src/main/java/com/shawnix/codepadx/repository/UserRepository.java
@@ -5,7 +5,13 @@ import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsUserByUsername(String username);
+
+    Optional<User> findUserByEmail(String email);
+
+    Optional<User> findById(Long userId);
 }
